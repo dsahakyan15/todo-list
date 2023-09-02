4d8eed3f712e3cf2975863b6916146f75ad48107
David Sahakyan <davitsahakyan373@gmail.com>
2023-09-02 19:50:10 +0400

    impo

--- src/components/TodoList/TodoListItem/TodoListItem.js
+++ src/components/TodoList/TodoListItem/TodoListItem.js
@@ -1,28 +1,55 @@
-import './todo-list-item.css';
-import {FaTrash,FaInfo,FaCheck} from 'react-icons/fa6'
+import { Component } from 'react'
 
-const TodoListItem = ({ text, important }) => {
-  const style = {
-    color: important ? "red" : "black",
-    fontWeight:important ? "bold" : 'normal'
+import './todo-list-item.css';
+import { FaTrash, FaInfo, FaCheck } from 'react-icons/fa6'
+
+class TodoListItem extends Component {
+  state={
+    isDone:false,
+    isImpo: this.props.important
   }
 
-  return (
-    <li className='list-item' style={style}>
+  onDone = () => {
+    this.setState({
+      isDone:!this.state.isDone
+    });
+    console.log("----Done", this.props.text)
+  }
+  onImpo = () => {
+    this.setState({
+      isImpo:!this.state.isImpo
+    })
+    console.log(this.props.text,'---Now is Important')
+  }
 
-      <span className='item-text'>
-      {text}        
-      </span>
+  render() {
+    const { text } = this.props
+    const { isDone,isImpo } = this.state
 
-      <span className='item-btns'>
-        <button className='item-btn-done'><FaCheck/></button>
-        <button className='item-btn-important'><FaInfo/></button>
-        <button className='item-btn-remove'>
-          <FaTrash/>
-        </button>
-      </span>
-    </li>
-  );
+
+    const textStyle = {
+      textDecoration:isDone ? 'line-through' : 'none',
+      color: isDone ? "#aaa" : isImpo ? "red" : "black",
+      fontWeight:isDone ? "normal" : isImpo ? "bold" : 'normal'
+    }
+
+    return (
+      <li className='list-item'>
+
+        <span className='item-text' style={textStyle}>
+          {text}
+        </span>
+
+        <span className='item-btns'>
+          <button className='item-btn-done' onClick={this.onDone}><FaCheck /></button>
+          <button className='item-btn-important' onClick={this.onImpo}><FaInfo /></button>
+          <button className='item-btn-remove'>
+            <FaTrash />
+          </button>
+        </span>
+      </li>
+    );
+  }
 }
-  
+
 export default TodoListItem;
